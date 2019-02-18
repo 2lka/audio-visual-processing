f36a6286f778a2ec6796633502dcdf6b20038564
2lka <dr.harm2@mail.ru>
2019-02-19 02:30:04 +0300

    l1 fixed

--- labs/l1/l1#1.py
+++ labs/l1/l1#1.py
@@ -1,17 +1,61 @@
 from PIL import Image
 
-image = Image.open('/home/dima/Desktop/Attachment.bmp')
-image2 = image.copy()
-size = image.size
-M, N = 20, 1/30
-K = M*N
 
-new_size = size
-for i in [M, N]:
-    new_size = [j*i for j in new_size]
-    image.thumbnail(new_size)
+def resize_rows(image, new_size):
+    new_image = Image.new("RGB", (new_size, image.size[1]), (0, 0, 0))
+    pix = image.load()
+    new_pix = new_image.load()
+    for i in range(image.size[1]):
+        counter = 0
+        remainder = 1
+        for j in range(image.size[0]):
+            colour = pix[j, i]
+            power = new_size / image.size[0]
+            while power and counter < new_size:
+                m = min(power, remainder)
+                new_pix[counter, i] = tuple(int(colour[k] * m + new_pix[counter, i][k]) for k in range(3))
+                power = power - m
+                remainder = remainder - m
+                if not remainder:
+                    counter = counter + 1
+                    remainder = 1
+    return new_image
+
+
+def resize_columns(image, new_size):
+    new_image = Image.new("RGB", (image.size[0], new_size), (0, 0, 0))
+    pix = image.load()
+    new_pix = new_image.load()
+    for i in range(image.size[0]):
+        counter = 0
+        remainder = 1
+        for j in range(image.size[1]):
+            colour = pix[i, j]
+            power = new_size / image.size[1]
+            while power and counter < new_size:
+                m = min(power, remainder)
+                new_pix[i, counter] = tuple(int(colour[k] * m + new_pix[i, counter][k]) for k in range(3))
+                power = power - m
+                remainder = remainder - m
+                if not remainder:
+                    counter = counter + 1
+                    remainder = 1
+    return new_image
+
+
+def resize_image(image, new_size):
+    return resize_rows(resize_columns(image, new_size[0]), new_size[1])
+
+
+image = Image.open('/home/dima/PycharmProjects/audio-visual-processing/labs/l1/test.bmp')
 image.show()
+M, N = 2, 1/3
 
-new_size2 = [i*K for i in size]
-image2.thumbnail(new_size2)
-image2.show()
+Ms = tuple(int(M * image.size[i]) for i in range(2))
+Ks = tuple(int(N * Ms[i]) for i in range(2))
+
+new_image = resize_image(resize_image(image, Ms), Ks)
+new_image.show()
+
+new_image = resize_image(image, Ks)
+new_image.show()
--- labs/l1/l1#2.py
+++ labs/l1/l1#2.py
@@ -1,17 +1,15 @@
 from PIL import Image
 import numpy as np
 
-image = Image.open('/home/dima/Desktop/Attachment.bmp')
+image = Image.open('/home/dima/PycharmProjects/audio-visual-processing/labs/l1/triangle.bmp')
 pix = image.load()
 size = image.size
 lightness = np.zeros(size)
-
 for i in range(size[0]):
     for j in range(size[1]):
-        a, b, c = pix[i, j]
-        pix[i, j] = 0, 0, c
-        lightness[i, j] = (a + b + c) // 3
-image.save('/home/dima/Desktop/bAttachment.bmp')
+        r, g, b, a = pix[i, j] #используется RGBA
+        lightness[i, j] = (r + g + b) // 3
+        pix[i, j] = int(lightness[i, j]), int(lightness[i, j]), int(lightness[i, j])
+image.save('/home/dima/PycharmProjects/audio-visual-processing/labs/l1/new_triangle.bmp')
 image.show()
 print(lightness)
-
